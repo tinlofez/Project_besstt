4a92c92cfc87c0d3f03b9f9db25877ac52fde24b
tinlofez <tinnlofr@gmail.com>
2026-07-01 01:31:01 +0700

    Add new images to Picture component
    Updated the images array to include new image paths.

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 import SectionWrapper from './SectionWrapper';
 // Add your own images by putting them in the assets folder and import them.
 const images = [
- picture,
- picture,
- picture,
- picture,
+ picture,src/assets/80d88dce-69f6-4b77-a8c1-636bc9156143.jpg
+ picture,src/assets/66b882d8-a51d-4b4e-8796-4f703ec1966f.jpg
+ picture,src/assets/504b5a53-ea63-422e-ae3f-5175d5fc4af8.jpg
+ picture,src/assets/5e9f234b-fbb4-4b8c-9846-d181fd9f3330.jpg
 
 ];
 function Picture() {
